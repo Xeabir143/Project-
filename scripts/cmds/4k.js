41352f44f96aa1f1a4b19f88d2c460be081d4ede
Jatirabbu <161809521+Jatirabbu@users.noreply.github.com>
2024-03-09 11:23:29 +0600

    Update 4k.js

--- scripts/cmds/4k.js
+++ scripts/cmds/4k.js
@@ -6,7 +6,7 @@ module.exports = {
     name: "upscaleai",
     aliases: ["4k", "upscale"],
     version: "1.0",
-    author: "aminul sordar",
+    author: "JARIF",
     countDown: 15,
     role: 0,
     longDescription: "Upscale your image.",
