215aaab12131c4107fcd87ce3335f4fc3be5eb27
Xeabir143 <165151391+Xeabir143@users.noreply.github.com>
2024-03-29 04:42:42 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[Siam islam]";
+const doNotDelete = "[THE FOX BOX]";
 /** 
 * @author ĊŸBËŖ ​​ÄŖMŸ
 * @author: do not delete it
