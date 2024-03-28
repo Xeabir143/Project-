6588963ae207b181a1fe094e23a36f233fce0bfe
Xeabir143 <165151391+Xeabir143@users.noreply.github.com>
2024-03-28 11:59:16 +0600

    Update Uptime.js

--- scripts/cmds/Uptime.js
+++ scripts/cmds/Uptime.js
@@ -1,54 +1,28 @@
-const moment = require('moment-timezone');
-moment.tz.setDefault('Asia/Jakarta');
-const os = require('os');
-
 module.exports = {
   config: {
-    name: "up",
-    aliases: ["stats"],
+    name: "uptime",
+    aliases: ["up", "upt"],
     version: "1.0",
-    countDown: 10,
-    author: "Hadi V", //jangan diganti ya
+    author: "XyryllPanget",
     role: 0,
     shortDescription: {
-      en: "status bot"
+      en: "Displays the uptime of the bot."
     },
     longDescription: {
-      en: "status bot"
+      en: "Displays the amount of time that the bot has been running for."
     },
-    category: "system",
+    category: "box chat",
     guide: {
-      en: "{pn}"
+      en: "Use {p}uptime to display the uptime of the bot."
     }
   },
-  
-onStart: async function ({ message, event, usersData, threadsData }) {
-     const uptime = process.uptime();
-     const startTime = Date.now();
-     const hours = Math.floor(uptime / 3600);
-     const minutes = Math.floor((uptime % 3600) / 60);
-     const second = Math.floor(uptime % 60);
-     
-     const arif = `${hours} hours ${minutes} minutes ${second} second`;
-     
-     const edi = "facebook.com/61555872531277";
-     const vania = await global.utils.getStreamFromURL("https://tinyurl.com/2cd86psc");
-   
-     const now = moment();
-     const riley = now.format('DD-MMMM-Y/hh.mm.ss A');
-     
-     const veli = `${Math.round(os.totalmem() / (1024 * 1024 * 1024))} GB`;
-     const putra = await usersData.getAll();
-     const loufi = await threadsData.getAll(); 
-     const luxion = `${os.type()} ${os.release()}`;
-     const rizky = `${os.cpus()[0].model} (${os.cpus().length} cores)`
-     
-     const endTime = Date.now();
-     const raffa = endTime - startTime;
-     
-     const hadi = `${arif}\n━━━━━━━━━━━\nBot Ping: ${raffa}\nTotal User: ${putra.length}\nTotal Grup: ${loufi.length}\nMemory: ${veli}\nOS: ${luxion}\nCPU: ${rizky}\n\nWaktu: ${riley}`
-
-     message.reaction("🐸", event.messageID);
-     message.reply({ body: hadi, attachment: vania }, event.threadID);
-  },
+  onStart: async function ({ api, event, args }) {
+    const uptime = process.uptime();
+    const seconds = Math.floor(uptime % 60);
+    const minutes = Math.floor((uptime / 60) % 60);
+    const hours = Math.floor((uptime / (60 * 60)) % 24);
+    const days = Math.floor(uptime / (60 * 60 * 24));
+    const uptimeString = `${hours} hours ${minutes} minutes ${seconds} second`;
+    api.sendMessage(`hello user, the bot has been running for ${uptimeString}.`, event.threadID);
+  }
 };
